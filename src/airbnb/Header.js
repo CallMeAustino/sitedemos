30c0ea1540171089f5aa790665dfbf88d481afd6
Austin <austin.hokun.wong@gmail.com>
2020-10-20 19:57:31 -0400

    added header css

--- src/airbnb/Header.js
+++ src/airbnb/Header.js
@@ -2,21 +2,29 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import LanguageIcon from '@material-ui/icons/Language';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import PersonIcon from '@material-ui/icons/Person';
 
 function Header() {
     return (
         <div className="header__container">
+
             <img
                 className="header__icon"
                 src="https:/i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
                 alt="logo"
             />
-            <section className='header_center'>
+
+            <section className='header__center'>
                 <input type="text" />
                 <SearchIcon />
             </section>
-            <section>
+
+            <section className='header__right'>
+                <p>Become a host</p>
                 <LanguageIcon />
+                <ExpandMoreIcon />
+                <PersonIcon />
             </section>
 
         </div>
