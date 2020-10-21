e09cf389bc86be4c8db8821a5eeefdc04d629002
Austin <austin.hokun.wong@gmail.com>
2020-10-21 14:27:00 -0400

    added footer blueprint

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Home from './airbnb/Home.js';
 import Search from './airbnb/Search.js';
 import Header from './airbnb/Header.js';
-
+import Footer from './airbnb/Footer.js';
 
 function App() {
   return (
     <>
       <Header />
       <Home />
+      <Footer />
     </>
   );
 }
